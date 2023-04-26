2afdf8a4c509019f3f306b6dafabc02c2b5c165c
Crowdbotics <team@crowdbotics.com>
2023-04-26 22:22:17 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "withered-feather-40244",
+  url: "https://withered-feather-40244.botics.co",
+  api: "https://withered-feather-40244.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
